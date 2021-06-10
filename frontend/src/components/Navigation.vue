a98069cc74f42e11d373e17047d0c375d289d06e
CarlosAmos <carlos.amos1@gmail.com>
2021-06-10 22:05:49 +1200

    Update Navigation

---
+++ frontend/src/components/Navigation.vue
@@ -0,0 +1,23 @@
+<template>
+<v-card>
+  <v-toolbar class='NavBar' color='cyan' dark flat>
+
+  </v-toolbar>
+</v-card>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+.NavBar {
+  color: aqua!important;
+  background-color: aqua!important;
+  border-radius: 40px!important;
+  display:flex!important;
+  width:90%;
+}
+</style>
