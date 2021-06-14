7ccc4d8347374b38327e2639356abfc3e0e91253
CarlosAmos <carlos.amos1@gmail.com>
2021-06-14 17:09:02 +1200

    Working createlisting

---
+++ frontend/src/components/ListingForm.vue
@@ -0,0 +1,113 @@
+<template>
+  <validation-observer
+    ref="observer"
+    v-slot="{ invalid }"
+  >
+    <form @submit.prevent="createListing">
+        <!-- Listing Name input Field-->
+      <validation-provider
+        v-slot="{ errors }"
+        name="Listing Name"
+        rules="required|max:100"
+      >
+        <v-text-field
+          v-model="listingData.listingName"
+          :counter="100"
+          :error-messages="errors"
+          label="Listing Name"
+          required
+        ></v-text-field>
+      </validation-provider>
+      <!-- Donator Name input Field-->
+        <validation-provider
+        v-slot="{ errors }"
+        name="Your Name"
+        rules="required|max:10"
+      >
+        <v-text-field
+          v-model="listingData.donator"
+          :counter="60"
+          :error-messages="errors"
+          label="Your Name / Organisation"
+          required
+        ></v-text-field>
+      </validation-provider>
+      <!-- Items input Field-->
+          <v-textarea v-model="listingData.items" clearable clear-icon="mdi-close-circle" label="List of items in listing"></v-textarea>
+      <!-- Description Name input Field-->
+          <v-textarea v-model="listingData.description" clearable clear-icon="mdi-close-circle" label="Description of listing"></v-textarea>
+      <v-btn class="mr-4" type="submit" :disabled="invalid">Create Listing</v-btn>
+      <v-btn @click="clear">
+        clear
+      </v-btn>
+    </form>
+  </validation-observer>
+</template>
+
+<script>
+import axios from "axios"
+  import { required, digits, email, max, regex } from 'vee-validate/dist/rules'
+  import { extend, ValidationObserver, ValidationProvider, setInteractionMode } from 'vee-validate'
+
+  setInteractionMode('eager')
+
+  extend('digits', {
+    ...digits,
+    message: '{_field_} needs to be {length} digits. ({_value_})',
+  })
+
+  extend('required', {
+    ...required,
+    message: '{_field_} can not be empty',
+  })
+
+  extend('max', {
+    ...max,
+    message: '{_field_} may not be greater than {length} characters',
+  })
+
+  extend('regex', {
+    ...regex,
+    message: '{_field_} {_value_} does not match {regex}',
+  })
+
+  extend('email', {
+    ...email,
+    message: 'Email must be valid',
+  })
+
+  export default {
+    components: {
+      ValidationProvider,
+      ValidationObserver,
+    },
+    data() {
+        return {
+            listingData :{
+                listingName: '', 
+                donator: '',
+                items: '',
+                description: ''
+            }
+        }
+    },
+
+    methods: {
+      submit () {
+        this.$refs.observer.validate()
+      },
+      createListing () {
+          axios.post('http://localhost:4200/api/listing', this.listingData)
+          .then(res=> console.log(res.data))
+          .catch(err => console.log(err))
+      },
+      clear () {
+        this.listingData.listingName = ''
+        this.listingData.donator = ''
+        this.listingData.items = ''
+        this.listingData.description = ''
+        this.$refs.observer.reset()
+      },
+    },
+  }
+</script>
--- frontend/src/views/Store/EditListing.vue
+++ frontend/src/views/Store/EditListing.vue
@@ -1,12 +1,34 @@
 <template>
   <div>
+        <div class="container">
       <h1>Edit Listing</h1>
+      <div class='row'>
+        <div class='col-2'>
+
+        </div>
+                <div class='col-8'>
+          <listing-form></listing-form>
+        </div>
+                <div class='col-2'>
+
+        </div>
+      </div>
+      <div class='row'>
+      <div class='col-12'>
+      </div>
+      </div>
+
+  </div>
   </div>
 </template>
 
 <script>
-export default {
+import ListingForm from '../../components/ListingForm.vue'
 
+export default {
+components: {
+  ListingForm
+}
 }
 </script>
 
